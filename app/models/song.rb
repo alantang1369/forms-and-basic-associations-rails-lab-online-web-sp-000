55c34252af3f0bec2ee5c9476309f6dfec98c5fc
Alan Tang <giabao_tang@yahoo.com>
2021-03-03 11:05:21 -0800

    commit tree 59436b2e08d3460f9ae62d72f23fdd51f202d091

--- app/models/song.rb
+++ app/models/song.rb
@@ -14,14 +14,15 @@ class Song < ActiveRecord::Base
 
   def note_contents=(contents)
     contents.each do |content|
-      if content.strip != ""
-         self.notes.build(content: content)
+      if !content.empty?
+         self.notes.build(content: content.strip)
       end
     end
   end
 
   def note_contents
-    self.notes.map(&:content)
+    #self.notes.map(&:content)
+    self.notes.map {|note| note.content}.join(" ")
   end
 
 end
